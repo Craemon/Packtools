2efec4b64dda97e135ba207b69e0dca66ae882f4
Craemon <121398546+Craemon@users.noreply.github.com>
2026-02-08 23:32:27 +0100

    chore: 1.1.1 install scripts

--- src/main/java/com/craemon/packtools/Main.java
+++ src/main/java/com/craemon/packtools/Main.java
@@ -18,7 +18,7 @@ import java.nio.file.Paths;
 )
 public class Main implements Runnable {
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
 
     // Make configPath optional, no 'required = true'
     @CommandLine.Option(names = {"-c", "--config"}, description = "Path to config JSON file")
